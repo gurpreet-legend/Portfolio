47ee5e5c3c24caab9ba74f7692114121900a405a
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-25 05:54:32 +0530

    logo.js done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import { Box, Container, Heading } from '@chakra-ui/layout'
 import React from 'react'
 
 const Page = () => {
     return (
-        <div>
-            Hello
-        </div>
+        <Container maxW="container.xl">
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align='center'>
+                Hello, I&apos;m a full-stack developer based in India
+            </Box>
+            
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Gurpreet Singh
+                    </Heading>
+                    <p>Digital Craftzman (Web Developer / Designer / Anime-lover)</p>
+                </Box>
+            </Box>
+
+        </Container>
     )
 }
 
