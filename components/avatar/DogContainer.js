608d5d62f714213e549414c48addb0c679dc2cbe
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-11-26 06:39:57 +0530

    pls hojaa

--- components/avatar/DogContainer.js
+++ components/avatar/DogContainer.js
@@ -1,20 +1,20 @@
-// import { forwardRef } from 'react'
+import { forwardRef } from 'react'
 import { Box } from '@chakra-ui/react'
 
-const DogContainer = ({children}) => (
-    <Box
-      ref={ref}
-      className="voxel-dog"
-      m="auto"
-      mt={['-20px', '-60px', '-40px']}
-      mb={['-40px', '-140px', '-200px']}
-      w={[280, 480, 640]}
-      h={[280, 480, 640]}
-      position="relative"
-    >
-      {children}
-    </Box>
-)
+const DogContainer = forwardRef(({ children }, ref) => (
+  <Box
+    ref={ref}
+    className="voxel-dog"
+    m="auto"
+    mt={['-20px', '-60px', '-40px']}
+    mb={['-40px', '-140px', '-200px']}
+    w={[280, 480, 640]}
+    h={[280, 480, 640]}
+    position="relative"
+  >
+    {children}
+  </Box>
+))
 DogContainer.displayName="DogContainer"
 
 export default DogContainer
